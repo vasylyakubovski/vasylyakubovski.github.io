95436528e18e4b9f7bffd85b2893cb000d541728
vasylyakubovski <57590815+vasylyakubovski@users.noreply.github.com>
2021-04-30 19:56:15 +0300

    Add files via upload

---
+++ js/t29/1.js
@@ -0,0 +1,45 @@
+const slides = document.querySelector('.slides');
+
+const slideNum = document.querySelectorAll('.slide').length;
+
+const maxMargin = (slideNum-1)*600;    
+
+function slideRight(){
+    let prev =  getMargin();
+
+    if( prev > -maxMargin){
+        slides.style.marginLeft = `${prev-600}px`;
+    }else{
+
+        direction = "left";
+    }
+}
+function slideLeft(){
+    let prev =  getMargin();
+
+    if( prev < 0){
+        slides.style.marginLeft = `${prev+600}px`;
+    }else{
+
+        direction = "right";
+    }
+}
+function getMargin(){
+
+    let prev = Number(slides.style.marginLeft.replace("px", "") ) || 0;
+    return prev
+}
+
+let direction = "right";
+
+setInterval(function(){
+    if( direction === "right" ){
+
+        slideRight();
+    }
+    if( direction === "left" ){
+
+        slideLeft();
+    }
+    
+}, 5000)
