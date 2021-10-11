727997eb12d82e914f5ab9c201128c9550ae67ec
vasylyakubovski <57590815+vasylyakubovski@users.noreply.github.com>
2021-10-11 21:11:23 +0300

    Delete t2 directory

--- t2/1.js
+++
@@ -1,5 +0,0 @@
-const buy=15000;
-const garantia=3000;
-const dostavka=60;
-const total= buy + garantia + dostavka;
-console.log(`загальна вартість ${total}`);
--- t2/4.js
+++
@@ -1,6 +0,0 @@
-const width=4;
-const height=5;
-const S= width * height;
-const price= 300;
-const total= 300 * S; 
-console.log(`Площа кімнати з шириною - ${width}м та довжиною ${height}м рівна ${S} квадратних метрів. Загальна вартість ламінату для цієї кімнати за ціною ${price}грн за метр квадратний рівна - ${total}.`);
