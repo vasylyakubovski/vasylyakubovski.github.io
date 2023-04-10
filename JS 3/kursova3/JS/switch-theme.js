0848db699d75dc893cefec258f14f3100e8c0305
vasylyakubovski <57590815+vasylyakubovski@users.noreply.github.com>
2023-04-10 22:18:19 +0300

    Add files via upload

---
+++ JS 3/kursova3/JS/switch-theme.js
@@ -0,0 +1,58 @@
+//Це додати в html
+/* <li class="nav-item">
+        <a class="nav-link" aria-current="page"
+        onclick="switchTheme()" href="#/home" id="swith-theme"
+        >Dark</a>
+    </li>
+*/
+
+
+function navbarDark(){
+    const navbar = document.querySelector('.navbar');
+    navbar.classList.remove('navbar-light');
+    navbar.classList.remove('bg-light');
+    navbar.classList.add('navbar-dark');
+    navbar.classList.add('bg-dark');
+
+    document.getElementById("swith-theme").innerText = "Light";
+}
+
+function navbarLight(){
+    const navbar = document.querySelector('.navbar');
+    navbar.classList.add('navbar-light');
+    navbar.classList.add('bg-light');
+    navbar.classList.remove('navbar-dark');
+    navbar.classList.remove('bg-dark');
+
+    document.getElementById("swith-theme").innerText = "Dark";
+}
+
+function switchStyleDark(){
+    const navbar = document.querySelector('body');
+    navbar.classList.add('theme-dark');
+}
+
+function switchStyleLight(){
+    const navbar = document.querySelector('body');
+    navbar.classList.remove('theme-dark');
+}
+
+function switchToDark(){
+    navbarDark();
+    switchStyleDark();
+}
+
+function switchToLight(){
+    navbarLight();
+    switchStyleLight();
+}
+
+function switchTheme(){
+    const navbar = document.querySelector('body');
+    if(navbar.classList.contains('theme-dark')){
+        switchToLight()
+    }
+    else{
+        switchToDark()
+    }
+}
